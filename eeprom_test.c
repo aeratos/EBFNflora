2d70091060fe0db4b18e240b9d47dccda73a6661
Bong <mscmacher@gmail.com>
2018-02-24 11:16:56 +0100

    eeprom_test.c

--- eeprom_test.c
+++ eeprom_test.c
@@ -12,31 +12,9 @@ void printString(char* s){
 		UART_putChar(uart, (uint8_t) *s);
 }
 
-char* frequency = "125K";
-char* minLight = "50";
-char* maxLight = "700";
-char* minTemp = "15";
-char* maxTemp  = "55";
-char* maxPoll = "150";
-char* R1 = "13000";
-char* C1 = "1.009249522e-03";
-char* C2 = "2.378405444e-04";
-char* C3 = "2.019202697e-07";
-
 int main(void){
 	EEPROM_init();
-	uart = UART_init("uart_0",115200);
-	
-	set_EEPROM_prescaler(frequency);
-	set_EEPROM_minLight(minLight);
-	set_EEPROM_maxLight(maxLight);
-	set_EEPROM_minTemp(minTemp);
-	set_EEPROM_maxTemp(maxTemp);
-	set_EEPROM_maxPoll(maxPoll);
-	set_EEPROM_r1(R1);
-	set_EEPROM_c1(C1);
-	set_EEPROM_c2(C2);
-	set_EEPROM_c3(C3);
+	uart = UART_init("uart_0", 115200);
 	
 	while(1){
 		char msg[16];
